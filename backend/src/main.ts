8365c5cf507affd5e7fedb38401aa2505d94e2d1
Timo Scheuermann <timo.scheuermann@gmx.de>
2020-08-25 15:48:08 +0200

    https test

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface';
+import { NestApplication, NestFactory } from '@nestjs/core';
+import { readFileSync } from 'fs';
 import { AppModule } from './app.module';
 
+const KEY_PATH = '/etc/letsencrypt/live/api.timos.design/';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const httpsOptions: HttpsOptions = {};
+  let app: NestApplication;
+  try {
+    httpsOptions.cert = readFileSync(KEY_PATH + 'fullchain.pem');
+    httpsOptions.key = readFileSync(KEY_PATH + 'privkey.pem');
+    app = await NestFactory.create(AppModule, { httpsOptions, cors: true });
+  } catch (error) {
+    app = await NestFactory.create(AppModule, { cors: true });
+  }
   await app.listen(3000);
 }
+
 bootstrap();
