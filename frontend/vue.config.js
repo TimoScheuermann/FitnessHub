781ac61da2806a9f79da9eb54654db65f8b6bf5a
Timo Scheuermann <timo.scheuermann@gmx.de>
2020-08-25 14:39:34 +0200

    init

---
+++ frontend/vue.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  pwa: {
+    manifestOptions: {
+      display: 'fullscreen',
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      start_url: '.'
+    },
+    name: 'Newsroom',
+    themeColor: '#000',
+    msTileColor: '#ffffff',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black-translucent',
+    iconPaths: {
+      favicon32: 'pwa/favicon-32.png',
+      favicon16: 'pwa/favicon-16.png',
+      appleTouchIcon: 'pwa/splash/manifest-icon-512.jpg',
+      maskIcon: 'pwa/maskIcon.svg',
+      msTileImage: 'pwa/msTileImage-144.png'
+    }
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData:
+          "@import '@/scss/_variables.scss';\n@import '@/scss/_mixins.scss';"
+      }
+    }
+  }
+};
