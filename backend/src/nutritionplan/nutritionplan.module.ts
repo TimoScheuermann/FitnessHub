3a8e9b7961550443289ae55ab5cfc046a3af529e
Jan Eger <Jan.Eger@Online.de>
2020-08-27 14:00:02 +0200

    updated nutritionplan

--- backend/src/nutritionplan/nutritionplan.module.ts
+++ backend/src/nutritionplan/nutritionplan.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { NutritionplanController } from './nutritionplan.controller';
+import { NutritionplanService } from './nutritionplan.service';
+import {
+  Nutritionplan,
+  NutritionplanSchema,
+} from './schemas/Nutritionplan.schema';
 
 @Module({
-  controllers: [NutritionplanController]
+  imports: [
+    MongooseModule.forFeature([
+      { name: Nutritionplan.name, schema: NutritionplanSchema },
+    ]),
+  ],
+  controllers: [NutritionplanController],
+  providers: [NutritionplanService],
 })
 export class NutritionplanModule {}
