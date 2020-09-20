49a0b5c25a0f68ca607d8efa2effb72efa5b79b5
Jan Eger <Jan.Eger@Online.de>
2020-09-20 16:26:16 +0200

    fixed recipe.service.ts

--- backend/src/recipe/recipe.service.ts
+++ backend/src/recipe/recipe.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { TgbotService } from 'src/tgbot/tgbot.service';
+import { IUser } from 'src/user/interfaces/IUser';
 import { CreateRecipeDTO } from './dtos/CreateRecipe.dto';
 import { UpdateRecipeDTO } from './dtos/UpdateRecipe.dto';
 import { IRecipe } from './interfaces/IRecipe';
