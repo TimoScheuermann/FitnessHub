670bd2b46fb3b88ecb4ceaac0194d5a734ad297e
Salocinf <nicolas.fuerhaupter@me.com>
2020-10-08 17:24:33 +0200

    added training wiki and nutrition wiki

--- frontend/src/views/nutrition/NutritionWiki.vue
+++ frontend/src/views/nutrition/NutritionWiki.vue
@@ -1,6 +1,387 @@
 <template>
   <div class="nutrition-wiki" content>
     <h1>Wiki</h1>
+
+    <h2>Nährstoffe</h2>
+
+    <tl-grid>
+      <tc-card title="Ballaststoffe" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <h3>gut für:</h3>
+          <ul>
+            <li>Senken von Cholesterinspiegel</li>
+            <li>Regulierung von Blutzucker</li>
+            <li>Förderung der Darmbewegung</li>
+          </ul>
+          <h3>enthalten in:</h3>
+          <ul>
+            <li>Äpfel</li>
+            <li>Birnen</li>
+            <li>Beeren</li>
+            <li>Zitrusfrüchte</li>
+            <li>getrocknete Aprikosen/Pflaumen/Rosinen</li>
+            <li>Vollkorn</li>
+            <li>Brokkoli</li>
+            <li>Erbsen</li>
+            <li>Karotten</li>
+            <li>Kohl</li>
+            <li>Nüsse und Samen</li>
+          </ul>
+        </div>
+      </tc-card>
+      <tc-card title="Calcium" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <h3>gut für:</h3>
+          <ul>
+            <li>Haare, Zähne, Knochen</li>
+            <li>Muskeln</li>
+            <li>Nerven</li>
+            <li>Blutgerinnung</li>
+            <li>Entzündungen</li>
+          </ul>
+          <h3>enthalten in:</h3>
+          <ul>
+            <li>Milch</li>
+            <li>grünem Gemüse (Fenchel, Brokkoli, Grünkohl, ...)</li>
+            <li>Beeren</li>
+            <li>Kräuter</li>
+            <li>Nüsse</li>
+            <li>Vollkorn</li>
+            <li>Eier</li>
+          </ul>
+        </div>
+      </tc-card>
+      <tc-card title="Fette/Lipide" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <ul>
+            <li>
+              bringen mehr als das Doppelte an Energie wie Proteine oder
+              Kohlenhydrate
+            </li>
+            <li>gesättigte Fettsäuren: Fleisch, Butter, ...</li>
+            <li>ungesättigte Fettsäuren: Pflanzenöle, Fisch, ...</li>
+            <li>Trans-Fettsäure: Chips, ... --> SCHLECHT</li>
+          </ul>
+        </div>
+      </tc-card>
+      <tc-card title="Kohlenhydrate" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <ul>
+            <li>
+              komplexe Kohlenhydrate: Getreide, Vollkorn, Kartoffeln, ... -->
+              Zucker muss erst aufgespalten werden
+            </li>
+            <li>Einfach Zucker: Frucht- und Traubenzucker</li>
+            <li>Zweifachzucker: Haushalts- und Milchzucker</li>
+          </ul>
+        </div>
+      </tc-card>
+      <tc-card title="Magnesium" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <h3>gut für:</h3>
+          <ul>
+            <li>Muskeln</li>
+            <li>Nerven</li>
+            <li>Herz</li>
+            <li>Knochen, Zähne</li>
+            <li>Energiestoffwechsel</li>
+            <li>Elektrolytgleichgewicht</li>
+          </ul>
+          <h3>enthalten in:</h3>
+          <ul>
+            <li>Vollkorn</li>
+            <li>Hafer</li>
+            <li>grünes Gemüse</li>
+            <li>Nüsse</li>
+            <li>Samen, Kerne</li>
+            <li>Hülsenfrüchte</li>
+          </ul>
+        </div>
+      </tc-card>
+      <tc-card title="Makronährstoffe" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <h3>was ist das?:</h3>
+          <ul>
+            <li>Energielieferanten</li>
+            <li>Kohlenhydrate, Proteine, Fette</li>
+          </ul>
+        </div>
+      </tc-card>
+      <tc-card title="Mikronährstoffe" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <h3>was ist das?:</h3>
+          <ul>
+            <li>Depots in Knochen, Zähne, Haut, Leber und Muskulatur</li>
+            <li>Vitamine, Mineralstoffe, Spurenelemente</li>
+          </ul>
+        </div>
+      </tc-card>
+      <tc-card title="Omega 3" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <h3>gut für:</h3>
+          <ul>
+            <li>Entzüdungshemmend</li>
+            <li>Fließeigenschaft von Blut</li>
+            <li>Blutdruck senkend</li>
+            <li>Aufbau von Zellen</li>
+          </ul>
+          <h3>enthalten in:</h3>
+          <ul>
+            <li>Fisch (Lachs, Thunfisch, Forelle, Sardine, Makrele,..)</li>
+            <li>Raps-, Hanf-, Lein-, Walnuss-, Chiaöl</li>
+            <li>Rosenkohl</li>
+            <li>Avocado</li>
+            <li>Spinat</li>
+            <li>Bohnen</li>
+            <li>Nüsse</li>
+            <li>Samen</li>
+            <li>Soja</li>
+          </ul>
+        </div>
+      </tc-card>
+      <tc-card title="Präbiotika" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <h3>gut für:</h3>
+          <ul>
+            <li>Darmflora</li>
+            <li>Immunsystem</li>
+          </ul>
+          <h3>enthalten in:</h3>
+          <ul>
+            <li>Spargel</li>
+            <li>Getreide</li>
+            <li>Chicorée</li>
+            <li>Topinambur</li>
+            <li>Zwiebeln</li>
+            <li>Knoblauch</li>
+            <li>Schwarzwurzel</li>
+            <li>grüne Bananen</li>
+            <li>Bohnen</li>
+            <li>Erbsen</li>
+          </ul>
+        </div>
+      </tc-card>
+      <tc-card title="Probiotika" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <h3>gut für:</h3>
+          <ul>
+            <li>Darmflora</li>
+            <li>Immunsystem</li>
+          </ul>
+          <h3>enthalten in:</h3>
+          <ul>
+            <li>Joghurt</li>
+            <li>Käse</li>
+            <li>Sauerkraut</li>
+            <li>Sauerteig</li>
+            <li>Kefir</li>
+            <li>Miso</li>
+            <li>Kombucha</li>
+            <li>Tempeh</li>
+            <li>Buttermilch</li>
+          </ul>
+        </div>
+      </tc-card>
+      <tc-card title="Proteine/Eiweiß" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <ul>
+            <li>werden nicht lange vom Körper gespeichert</li>
+          </ul>
+        </div>
+      </tc-card>
+      <tc-card title="Vitamin B7 (Biotin)" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <h3>gut für:</h3>
+          <ul>
+            <li>Haut, Haare, Nägel</li>
+            <li>Stoffwechsel</li>
+          </ul>
+          <h3>enthalten in:</h3>
+          <ul>
+            <li>Eier</li>
+            <li>Nüsse</li>
+            <li>Haferflocken</li>
+            <li>Milch</li>
+            <li>Hefe</li>
+            <li>Leber</li>
+          </ul>
+        </div>
+      </tc-card>
+      <tc-card title="Vitamin B12" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <h3>wichtig für:</h3>
+          <ul>
+            <li>Stoffwechel</li>
+            <li>Abbau von Fettsäuren</li>
+          </ul>
+          <h3>enthalten in:</h3>
+          <ul>
+            <li>
+              nur in tierischen Nahrungsmitteln, da es nur von Mikroorganismen
+              synthetisiert werden kann
+            </li>
+          </ul>
+        </div>
+      </tc-card>
+      <tc-card title="Vitamin C" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <h3>gut für:</h3>
+          <ul>
+            <li>Immunsystem</li>
+            <li>Bindegewebe</li>
+            <li>Botenstoffe</li>
+            <li>Eisenaufnahme</li>
+            <li>Zellen</li>
+            <li>Haut, Zähne, Knochen</li>
+          </ul>
+          <h3>enthalten in:</h3>
+          <ul>
+            <li>Hagebutte</li>
+            <li>Sanddorn</li>
+            <li>Paprika</li>
+            <li>Kohl</li>
+            <li>Zitrusfrüchte</li>
+            <li>Beeren</li>
+            <li>Spinat</li>
+          </ul>
+        </div>
+      </tc-card>
+      <tc-card title="Vitamin D3" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <h3>gut für:</h3>
+          <ul>
+            <li>Kalziumaufnahme</li>
+            <li>Erhalt und Aufbau von Knochen</li>
+            <li>Zellstoffwechsel</li>
+            <li>Aufnahme über Sonnenlicht über die Haut!</li>
+          </ul>
+          <h3>enthalten in:</h3>
+          <ul>
+            <li>
+              kaum in Nahrung, jedoch: fetthaltiger Fisch (Lachs, Aal, Makrele,
+              Hering, ...)
+            </li>
+          </ul>
+        </div>
+      </tc-card>
+      <tc-card title="Vitamin K2" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <h3>gut für:</h3>
+          <ul>
+            <li>Einlagerung von Calcium in Knochen</li>
+            <li>-> Beugt Arteriosklerose (Arterienverkalkung) vor</li>
+            <li>-> Beugt Osteoporose (brüchige Knochen) vor</li>
+          </ul>
+          <h3>enthalten in:</h3>
+          <ul>
+            <li>Kräuter (Kresse, Schnittlauch)</li>
+            <li>grünes Blattgemüse</li>
+            <li>Rinderleber</li>
+            <li>Sojabohnen</li>
+            <li>Hafer</li>
+            <li>Vollweizen</li>
+            <li>Kartoffeln</li>
+            <li>Kohl</li>
+          </ul>
+        </div>
+      </tc-card>
+    </tl-grid>
+
+    <h2>Supplements:</h2>
+
+    <tl-grid>
+      <tc-card title="Whey" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <ul>
+            <li>aus Molkenprotein der Milch (natürliches Eiweiß)</li>
+            <li>leicht verdaulich, kurze Resorptionszeit</li>
+            <li>enthält alle unentbährlichen Aminosöuren</li>
+            <li>Whey-Konzentrat:</li>
+            <ul>
+              <li>hoher Eiweißgehalt (70-78%), günstig</li>
+            </ul>
+            <li>Whey-Isolat</li>
+            <ul>
+              <li>
+                hoher Eiweißgehalt (90-96%), geringer Fett- und
+                Kohlenhydrat-Anteil, laktosefrei
+              </li>
+            </ul>
+            <li>Whey-Hydrolysat</li>
+            <ul>
+              <li>
+                aus Whey-Konzentrat oder -Isolat gewonnen, aufgespaltete
+                Proteine für eine bessere Aufnahme
+              </li>
+            </ul>
+          </ul>
+        </div>
+      </tc-card>
+      <tc-card title="Casein" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <ul>
+            <li>Milchprotein</li>
+            <li>wird langsamer aufgenommen (eignet sich für Abends!)</li>
+          </ul>
+        </div>
+      </tc-card>
+      <tc-card title="Kreatin" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <ul>
+            <li>
+              Wassereinlagerungen in den Muskeln - lassen diese praller und
+              größer aussehen
+            </li>
+            <li>mehr Kraft und Kraftausdauer</li>
+            <li>weniger Muskelschäden</li>
+            <li>verkürzte Regenerationszeit</li>
+            <li>
+              Achtung: Achte darauf zusätzlich genug Flüssigkeit zu dir zu
+              nehmen!
+            </li>
+          </ul>
+        </div>
+      </tc-card>
+      <tc-card title="L-Glutamin" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <ul>
+            <li>Beschleunigung der Regenaration</li>
+            <li>schnellere Wiederauffüllung der Glykogen-Speicher</li>
+          </ul>
+        </div>
+      </tc-card>
+      <tc-card title="EAAs" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <ul>
+            <li>
+              essentielle Aminosäuren sich wichtg für das Immunsystem und den
+              Stoffwechsel
+            </li>
+            <li>können nicht vom Körper hergestellt werden</li>
+            <li>
+              sind am Aufbau von Proteinen und somit an anabolen Prozessen
+              beteiligt
+            </li>
+            <li>
+              werden nicht genügend EAAs zugeführt, nimmt sich der Organismus
+              dieses aus der eigenen Körpersubstanz
+            </li>
+          </ul>
+        </div>
+      </tc-card>
+      <tc-card title="BCAAs" :dark="$store.getters.darkmode">
+        <div class="card-content">
+          <ul>
+            <li>essentiellen Aminosäuren Valin, Leucin, Isoleucin</li>
+            <li>
+              werden nicht über die Leber verstoffwechselt, sondern direkt über
+              den Darm aufgenommen
+            </li>
+            <li>wichtig für die Muskelproteisynthese</li>
+          </ul>
+        </div>
+      </tc-card>
+    </tl-grid>
   </div>
 </template>
 
@@ -11,4 +392,10 @@ import { Vue, Component } from 'vue-property-decorator';
 export default class NutritionWiki extends Vue {}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.nutrition-wiki {
+  .card-content {
+    text-align: justify;
+  }
+}
+</style>
--- frontend/src/views/training/TrainingWiki.vue
+++ frontend/src/views/training/TrainingWiki.vue
@@ -1,6 +1,133 @@
 <template>
   <div class="training-wiki" content>
-    <h1>Wiki</h1>
+    <h1>Muskelguide</h1>
+    Du willst deinen Körper besser formen und dafür gezielt die Problemzonen
+    trainieren? Kein Problem. Wir haben unsere Übungen nach betroffenen Muskeln
+    sortiert – schau mal rein*. <br />* Falls du allerdings den
+    Anatomieunterricht geschwänzt hast und totaler Fitnessanfänger bist, lohnt
+    es sich diesen kurzen Muskelguide durchzulesen. Hier kannst du erfahren, wo
+    der aufgelistete Muskel sich befindet und warum er trainiert werden sollte.
+    <h2>Oberkörper</h2>
+    Im Gegensatz zu den Frauen, befinden sich die Baustellen der Männer meistens
+    in der oberen Körperhälfte. Der Wunsch nach definiertem Bauch und Brust,
+    starken Schultern und breitem Armumfang (= V-Form) lässt sich leichter
+    erfüllen, wenn du weißt welche Muskeln sich überhaupt im Oberkörper
+    befinden.
+    <br />
+    <br />
+    <tl-grid>
+      <tc-card title="Bizeps" :dark="$store.getters.darkmode">
+        <p>
+          DAS Statussymbol für echte Kerle. Befindet sich im Oberarm und hat
+          zwei Muskelstränge, die am besten gleich stark trainiert werden
+          sollten – sonst könnte es zu Muskelproblemen kommen, die infolge des
+          Ungleichgewichts in der Muskelstärke entstehen.
+        </p>
+      </tc-card>
+      <tc-card title="Deltamuskel" :dark="$store.getters.darkmode">
+        <p>
+          er gehört zu der Gruppe der Schulter- und Nackenmuskeln. Er „verpackt“
+          das Schultergelenk, verleiht der Schulter seine Kontur und bildet den
+          äußersten Punkt der V-Form. Er ist für die Hebung des Armes zur Seite
+          verantwortlich und kann bei Vernachlässigung ebenfalls Schmerzen in
+          den Oberarmen und Schulter verursachen.
+        </p>
+      </tc-card>
+      <tc-card title="Handstrecker" :dark="$store.getters.darkmode">
+        <p>
+          dieser Muskel ist für den sog. Tennisellenbogen verantwortlich und
+          befindet sich auf der Oberseite deines Unterarms.
+        </p>
+      </tc-card>
+      <tc-card title="Latissimus" :dark="$store.getters.darkmode">
+        <p>
+          ist der größte Skelettmuskel und befindet sich an der seitlichen
+          Rückenmuskulatur. Wenn du diesen trainierst, erscheint dein Rücken
+          breiter und erreichst so einfacher die gewünschte V-Form.
+        </p>
+      </tc-card>
+      <tc-card title="Pectoralis" :dark="$store.getters.darkmode">
+        <p>
+          Brustmuskel, der für definierte und starke Brustmuskulatur
+          verantwortlich ist.
+        </p>
+      </tc-card>
+      <tc-card title="Trapezmuskel" :dark="$store.getters.darkmode">
+        <p>
+          liegt im oberen, zentralen Bereich des Rückens und Nackens und trägt
+          zu deinem „Stiernacken“ bei.
+        </p>
+      </tc-card>
+      <tc-card title="Trizeps" :dark="$store.getters.darkmode">
+        <p>
+          ist der Gegenspieler vom Bizeps und befindet sich auf der Rückseite
+          des Oberarms.
+        </p>
+      </tc-card>
+      <tc-card title="Vorderer Sägemuskel" :dark="$store.getters.darkmode">
+        <p>
+          liegt seitlich unterhalb der Brust. Unter anderem ist der Muskel auch
+          als Boxer-Muskel bekannt. Bei diesen Sportlern ist er ständig im
+          Einsatz und deshalb auch besonders ausgeprägt.
+        </p>
+      </tc-card>
+    </tl-grid>
+
+    <h2>Unterkörper</h2>
+    Falls deine Problemzonen sich unterhalb der Bauchnabel befinden musst du
+    diese Muskelgruppen unbedingt kennen.
+    <br />
+    <br />
+    <tl-grid>
+      <tc-card title="Adduktor" :dark="$store.getters.darkmode">
+        <p>
+          ist die Innenseite deiner Oberschenkel. Bei Frauen sammeln sich die
+          meisten Naschereien hier an.
+        </p>
+      </tc-card>
+      <tc-card title="Abduktor" :dark="$store.getters.darkmode">
+        <p>
+          sind die äußeren seitlichen Bauch- und Beinmuskeln.
+        </p>
+      </tc-card>
+      <tc-card title="Beinbeuger" :dark="$store.getters.darkmode">
+        <p>
+          wichtig für den hinteren Teil der Oberschenkel. Wir empfehlen das
+          Training mit Beinbeuger besonders den Frauen, die Cellulite am
+          Oberschenkel und Po bekämpfen wollen.
+        </p>
+      </tc-card>
+      <tc-card title="Gluteus Maximus" :dark="$store.getters.darkmode">
+        <p>
+          einfach gesagt dein Gesäßmuskel. Wichtiges Trainingsziel für alle, die
+          einen knackigen Po haben wollen…oder alle die nicht mehr in die
+          Lieblingsjeans passen.
+        </p>
+      </tc-card>
+      <tc-card
+        title="Unterer Rücken (Rückenstrecker)"
+        :dark="$store.getters.darkmode"
+      >
+        <p>
+          besonders wichtig bei Menschen, die sich berufsbedingt wenig bewegen
+          und demnach unter Rückenschmerzen leiden. Durch gezieltes Training des
+          unteren Rückens wird die Muskulatur gestärkt und so Schmerzen
+          vorgebeugt.
+        </p>
+      </tc-card>
+      <tc-card title="Rumpf" :dark="$store.getters.darkmode">
+        <p>
+          umfasst den Brustkorb, Bauch, Rücken und Becken und seine schmalste
+          Stelle ist die Taille. Das Training stärkt die Körpermitte und beugt
+          Rückenbeschwerden vor.
+        </p>
+      </tc-card>
+      <tc-card title="Quadrizeps" :dark="$store.getters.darkmode">
+        <p>
+          befindet sich auf der Vorderseite des Oberschenkels.
+        </p>
+      </tc-card>
+    </tl-grid>
   </div>
 </template>
 
