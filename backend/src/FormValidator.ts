df09298db98678cd1e673ccc3ffc36d15c7f1f42
Timo Scheuermann <timo.scheuermann@gmx.de>
2021-02-23 18:26:12 +0100

    updated recipe creation validation

---
+++ backend/src/FormValidator.ts
@@ -0,0 +1,51 @@
+import { UnprocessableEntityException } from '@nestjs/common';
+
+export class FormValidator {
+  public static throwEx(key: string, message: string): void {
+    throw new UnprocessableEntityException({ key, message });
+  }
+
+  public static checkString(s: string, key: string, message: string): string {
+    if (!s || s.length === 0) {
+      this.throwEx(key, message);
+    }
+    return s;
+  }
+
+  public static checkStringArray(
+    array: string[],
+    key: string,
+    message: string,
+  ): string[] {
+    if (!array || array.length === 0) {
+      this.throwEx(key, message);
+    }
+
+    if (array.filter((x) => x.length === 0).length === 0) {
+      this.throwEx(key, message);
+    }
+
+    return array.filter((x) => x.length > 0);
+  }
+
+  public static checkNumber(n: number, key: string, message: string): number {
+    if (!n || typeof n !== 'number') {
+      this.throwEx(key, message);
+    }
+
+    return n;
+  }
+
+  public static checkNumberMinMax(
+    n: number,
+    min: number,
+    max: number,
+    key: string,
+    message: string,
+  ): number {
+    if (n < min || n > max) {
+      this.throwEx(key, message);
+    }
+    return n;
+  }
+}
--- backend/src/recipe/recipe.service.ts
+++ backend/src/recipe/recipe.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { FormValidator } from 'src/FormValidator';
 import { TgbotService } from 'src/tgbot/tgbot.service';
 import { IUser } from 'src/user/interfaces/IUser';
 import { CreateRecipeDTO } from './dtos/CreateRecipe.dto';
 import { UpdateRecipeDTO } from './dtos/UpdateRecipe.dto';
+import { INutrition } from './interfaces/INutrition';
 import { IRecipe } from './interfaces/IRecipe';
+import { IRecipeIngredient } from './interfaces/IRecipeIngredient';
 import { LikedRecipe } from './schemas/LikedRecipe.schema';
 import { Recipe } from './schemas/Recipe.schema';
 
@@ -73,27 +76,62 @@ export class RecipeService {
     user: IUser,
     createRecipeDTO: CreateRecipeDTO,
   ): Promise<IRecipe> {
-    if (
-      [
-        createRecipeDTO.title,
-        createRecipeDTO.category,
-        createRecipeDTO.thumbnail,
-        createRecipeDTO.steps,
-      ].filter((x) => x.length === 0).length > 0
-    ) {
-      return;
-    }
-    if (
-      !createRecipeDTO.time ||
-      createRecipeDTO.time <= 0 ||
-      !createRecipeDTO.nutrition ||
-      !createRecipeDTO.ingredients ||
-      !createRecipeDTO.difficulty
-    ) {
-      return;
-    }
+    let { category, steps } = createRecipeDTO;
+
+    const { difficulty, thumbnail, time, title } = createRecipeDTO;
+
+    FormValidator.checkString(title, 'Titel', 'Bitte gib einen Titel an');
+    FormValidator.checkString(
+      thumbnail,
+      'Vorschaubild',
+      'Bitte gib ein Vorschaubild an',
+    );
+
+    category = FormValidator.checkStringArray(
+      category,
+      'Kategorie',
+      'Bitte gib eine Kategorie an',
+    );
+
+    FormValidator.checkNumber(
+      difficulty,
+      'Schwierigkeit',
+      'Bitte gib eine Schwierigkeit an',
+    );
+    FormValidator.checkNumberMinMax(
+      difficulty,
+      1,
+      3,
+      'Schwierigkeit',
+      'Die Schwierigkeit muss zwischen 1 und 3 liegen',
+    );
+
+    steps = FormValidator.checkStringArray(
+      steps,
+      'Schritte',
+      'Bitte gib die Zubereitungsschritte an',
+    );
+
+    FormValidator.checkNumber(
+      time,
+      'Zeit',
+      'Bitte gib eine durchschnittliche Zubereitungszeit an',
+    );
+    FormValidator.checkNumberMinMax(
+      time,
+      1000,
+      Number.MAX_VALUE,
+      'Zeit',
+      'Die Zubereitungszeit kann nicht unter einer Sekunde liegen',
+    );
+
+    this.checkIngredients(createRecipeDTO.ingredients);
+    this.checkNutrition(createRecipeDTO.nutrition);
+
     const recipe = await this.recipeModel.create({
       ...createRecipeDTO,
+      steps: steps,
+      category: category,
       created: new Date().getTime(),
       updated: new Date().getTime(),
       author: user._id,
@@ -163,7 +201,63 @@ export class RecipeService {
     );
   }
 
-  public transformName(user: IUser): string {
+  private transformName(user: IUser): string {
     return [user.givenName, user.familyName].filter((x) => !!x).join(' ');
+  }
+
+  private checkIngredients(ing: IRecipeIngredient[]): boolean {
+    if (!ing || ing.length) {
+      FormValidator.throwEx('Zutaten', 'Bitte gib eine Zutatenliste an');
+    }
+    ing.forEach((x, i) => {
+      FormValidator.checkString(
+        x.name,
+        'Zutatenname',
+        'Der Name der Zutat #' + (i + 1) + 'fehlt',
+      );
+      FormValidator.checkString(
+        x.amount,
+        'Zutatenmenge',
+        'Die Mange die an ' + x.name + ' benötigt wird fehlt',
+      );
+      FormValidator.checkString(
+        x.unit,
+        'Zutateneinheit',
+        'Die Einheit der Menge der Zutat ' + x.name + ' fehlt',
+      );
+    });
+    return true;
+  }
+
+  private checkNutrition(nut: INutrition[]): boolean {
+    if (!nut || nut.length) {
+      FormValidator.throwEx('Nährwerte', 'Bitte gib eine Nährtwertliste an');
+    }
+    nut.forEach((x, i) => {
+      FormValidator.checkString(
+        x.title,
+        'Nährwertname',
+        'Der Name des Nährwerts #' + (i + 1) + 'fehlt',
+      );
+      FormValidator.checkNumber(
+        x.amount,
+        'Nährwertmenge',
+        'Die Mange des Nährwerts ' + x.title + ' fehlt',
+      );
+
+      FormValidator.checkNumberMinMax(
+        x.amount,
+        0,
+        Number.MAX_VALUE,
+        'Nährwertmenge',
+        'Die Nährwertsmenge von ' + x.title + ' kann nicht kleiner als 0 sein',
+      );
+      FormValidator.checkString(
+        x.unit,
+        'Nährwerteinheit',
+        'Die Einheit des Nährwerts ' + x.title + ' fehlt',
+      );
+    });
+    return true;
   }
 }
