c7b972575e964054f197306c2b663d1855079c08
Timo Scheuermann <timo.scheuermann@gmx.de>
2021-03-01 12:04:58 +0100

    fixed header for nutrition tab

--- frontend-redesign/src/views-interim/Nutrition-Interim.vue
+++ frontend-redesign/src/views-interim/Nutrition-Interim.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="interim-nutrition">
     <FHHeader
-      backTitle="Ernährung"
-      rootRoute="nutrition"
+      :backTitle="$route.meta.backTitle || 'Ernährung'"
+      :rootRoute="$route.meta.backRoute || 'nutrition'"
       :title="$route.params.category || $route.meta.hero"
     />
 
