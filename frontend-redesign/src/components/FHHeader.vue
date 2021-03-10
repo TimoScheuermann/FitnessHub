c56ab96f2873cdb202c4edbf11c8f45b066cfcc5
Timo Scheuermann <timo.scheuermann@gmx.de>
2021-03-10 21:05:56 +0100

    fixed header

--- frontend-redesign/src/components/FHHeader.vue
+++ frontend-redesign/src/components/FHHeader.vue
@@ -96,7 +96,7 @@ export default class FHHeader extends Vue {
     position: absolute;
     left: 50%;
     top: 50%;
-    transform: translate(-50%, -50%);
+    transform: translate(-50%, calc(-50% + env(safe-area-inset-top) / 2));
 
     overflow: hidden;
     display: -webkit-box;
